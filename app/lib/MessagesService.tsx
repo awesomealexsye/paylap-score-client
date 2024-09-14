96dbf9c973e0d9fe89713754ba8070157cbbc137
ARBAZ <arbaz@Arbaz.local>
2024-09-14 23:17:08 +0530

    user kyc button added

--- app/lib/MessagesService.tsx
+++ app/lib/MessagesService.tsx
@@ -5,8 +5,9 @@ import { ToastAndroid } from "react-native";
 const MessagesService = {
     commonMessage(message: string, messageType: string = "ERROR", duration = ToastAndroid.SHORT) {
         ToastAndroid.show(message, duration)
-    }
+    },
 }
+
 
 
 
--- app/screens/Profile/UserKyc.tsx
+++ app/screens/Profile/UserKyc.tsx
@@ -1,132 +1,115 @@
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import RBSheet from 'react-native-raw-bottom-sheet';
 import { useTheme } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react'
-import { View, Text, TouchableOpacity, Image, ScrollView, StyleSheet } from 'react-native'
+import LoginSheet from '../../components/BottomSheet/LoginSheet';
 import { GlobalStyleSheet } from '../../constants/StyleSheet';
-import { IMAGES } from '../../constants/Images';
-import { COLORS, FONTS } from '../../constants/theme';
-import { StackScreenProps } from '@react-navigation/stack';
-import { RootStackParamList } from '../../navigation/RootStackParamList';
 import Header from '../../layout/Header';
-import { FontAwesome } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
 import Input from '../../components/Input/Input';
+import { FontAwesome } from '@expo/vector-icons';
 import Button from '../../components/Button/Button';
 import { MessagesService } from '../../lib/MessagesService';
+import AadhaarOtp from './AadhaarOtp';
+import { COLORS, FONTS } from '../../constants/theme';
+
+type Props = {
+    height?: string,
+}
 
 
-type ProfileScreenProps = StackScreenProps<RootStackParamList, 'UserKyc'>;
-
-const UserKyc = ({ navigation }: ProfileScreenProps) => {
-
-
-
+const UserKyc = forwardRef((props, ref) => {
 
     const theme = useTheme();
     const { colors }: { colors: any } = theme;
 
-    const [aadhaar, setAadhaar] = useState("");
+    const refRBSheet = useRef<any>(null);
 
+    const [isSheet, setIsSheet] = useState(false);
+    const [aadhaar, setAadhaar] = useState("");
+    useImperativeHandle(ref, () => ({
+
+        openSheet: async (value: string) => {
+            await refRBSheet.current.open();
+        },
+        closeSheet() {
+            refRBSheet.current.close();
+        }
+
+    }));
+
+    const handleSheet = async () => {
+        await refRBSheet.current.open();
+    }
     const sendOtp = () => {
+        console.log(aadhaar)
         if (aadhaar.length != 12) {
-            MessagesService.commonMessage("Aadhaar Digit must be 12 digits");
+            MessagesService.commonMessage("Invalid Aadhaar Number")
         } else {
-            console.log("send otp", aadhaar)
+            handleSheet();
         }
 
     }
+
     return (
-        <View style={{ backgroundColor: colors.card, flex: 1 }}>
-            <Header
-                title='Update KYC'
-                leftIcon={'back'}
-            />
-            <ScrollView showsHorizontalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1, paddingBottom: 50 }}>
-                <View style={[GlobalStyleSheet.card, { backgroundColor: colors.card }, { marginTop: 20 }]}>
-                    <View style={[GlobalStyleSheet.cardHeader, { borderBottomColor: COLORS.inputborder }]}>
-                        <Text style={{ ...FONTS.fontMedium, fontSize: 14, color: colors.title, textAlign: 'center' }}>Verify Your Aadhaa Card & PanCard</Text>
-                    </View>
-                    <View style={GlobalStyleSheet.cardBody}>
-                        <View style={{ marginBottom: 10 }}>
-                            <Input
-                                inputRounded
-                                icon={<FontAwesome style={{ opacity: .6 }} name={'address-card'} size={20} color={colors.text} />}
-                                //value={''}  
-                                placeholder="Your Aadhaar Card Number"
-                                onChangeText={(value) => setAadhaar(value)}
-                            />
+        <>
+            <RBSheet
+                ref={refRBSheet}
+                closeOnDragDown={true}
+                height={300}
+                openDuration={100}
+                customStyles={{
+
+                    container: {
+                        backgroundColor: theme.dark ? colors.background : colors.cardBg,
+                    },
+                    draggableIcon: {
+                        marginTop: 10,
+                        marginBottom: 5,
+                        height: 5,
+                        width: 80,
+                        backgroundColor: colors.border,
+                    }
+                }}
+            >
+                {<AadhaarOtp value={aadhaar} sheetRef={refRBSheet} />}
+
+            </RBSheet>
+
+            <View style={{ flex: 1, backgroundColor: colors.background }}>
+                <View style={{}}>
+                    <Header
+                        title={'User Kyc'}
+                        leftIcon={'back'}
+                        titleRight
+                    />
+                    <ScrollView>
+                        <View style={[GlobalStyleSheet.container, { padding: 0, paddingTop: 10 }]}>
+                            <View style={{ marginTop: 20, }}>
+                                <View style={[GlobalStyleSheet.cardHeader, { borderBottomColor: COLORS.inputborder }]}>
+                                    <Text style={{ ...FONTS.fontMedium, fontSize: 14, color: colors.title, textAlign: 'center' }}>Your Aadhaar Card Number</Text>
+                                </View>
+                                <View style={{ marginTop: 20 }}>
+                                    <View style={{ marginBottom: 10 }}>
+                                        <Input
+                                            inputRounded
+                                            icon={<FontAwesome style={{ opacity: .6 }} name={'address-card'} size={20} color={colors.text} />}
+                                            placeholder="Your Aadhaar Card Number"
+                                            onChangeText={(value) => setAadhaar(value)}
+                                        />
+                                    </View>
+
+                                    <View style={GlobalStyleSheet.cardBody}>
+                                        <Button title={'Sent OTP'} onPress={() => { sendOtp() }} />
+
+                                    </View>
+                                </View>
+                            </View>
                         </View>
-                        <View style={GlobalStyleSheet.cardBody}>
-                            <Button title={'Sent OTP'} onPress={sendOtp} />
-
-                        </View>
-
-                        {/* <View style={{marginBottom:10}}>
-                                    <Input
-                                        inputRounded
-                                        icon={<MaterialIcons style={{opacity:.6}} name={'email'} size={20} color={colors.text}/>}
-                                        //value={''}  
-                                        placeholder="Enter Email"
-                                        onChangeText={(value)=> console.log(value)}
-                                    />
-                                </View> */}
-
-                    </View>
+                    </ScrollView>
                 </View>
+            </View>
+        </>
+    );
+});
 
-            </ScrollView>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    arrivaldata: {
-        backgroundColor: COLORS.card,
-        borderRadius: 20,
-        //width:'100%',
-        paddingHorizontal: 15,
-        borderWidth: 1,
-        borderColor: '#EFEFEF',
-    },
-    sectionimg: {
-        height: 104,
-        width: 104,
-        borderRadius: 150,
-        backgroundColor: COLORS.primary,
-        overflow: 'hidden',
-        marginBottom: 25
-    },
-    brandsubtitle2: {
-        ...FONTS.fontRegular,
-        fontSize: 12
-    },
-    brandsubtitle3: {
-        ...FONTS.fontMedium,
-        fontSize: 12,
-        color: COLORS.title
-    },
-    profilecard: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 15,
-        marginRight: 10,
-        marginBottom: 20
-    },
-    cardimg: {
-        height: 54,
-        width: 54,
-        borderRadius: 55,
-        backgroundColor: COLORS.card,
-        shadowColor: "rgba(0,0,0,0.5)",
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.34,
-        shadowRadius: 18.27,
-        elevation: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
-
-export default UserKyc
+export default UserKyc;
