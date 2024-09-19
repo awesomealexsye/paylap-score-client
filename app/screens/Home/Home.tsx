6575bd1b042e879654facd73f4f082c7aefea008
Vishal <vivanpanchal9540@gmail.com>
2024-09-19 14:11:29 +0530

    home conflict fic

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -13,7 +13,6 @@ import { openDrawer } from '../../redux/actions/drawerAction';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { ApiService } from '../../lib/ApiService';
 import { MessagesService } from '../../lib/MessagesService';
-import CommonService from '../../lib/CommonService';
 
 
 
@@ -43,16 +42,13 @@ export const Home = ({ navigation }: HomeScreenProps) => {
     const [customersData, setCustomersData] = useState<any>([]);
     const [homeBanner, setHomeBanner] = useState<any>({});
     const [filteredCustomers, setFilteredCustomers] = useState(customersData);
-    const [userName, setUsername] = useState("");
+
     useEffect(() => {
         fetchCustomerList();
-        CommonService.currentUserDetail().then((res) => {
-            setUsername(res?.name);
-        })
 
     }, [])
     const handleSearch = (text: string) => {
-        // console.log("handle search", text);
+        console.log("handle search", text);
         setSearchText(text);
         const filteredList = customersData.filter((customer: Customer) =>
             customer.name.toLowerCase().includes(text.toLowerCase())
@@ -62,16 +58,12 @@ export const Home = ({ navigation }: HomeScreenProps) => {
     const fetchCustomerList = async () => {
         const homeApiRes = await ApiService.postWithToken("api/shopkeeper/list-customer", {});
         if (homeApiRes.status == true) {
-            let customersApiData: Customer = homeApiRes?.data?.records ?? [];
+            let customersApiData: Customer = homeApiRes?.data?.records;
             let homeBanner = homeApiRes?.data?.shopkeeper_transaction_sum;
             setHomeBanner(homeBanner);
             setCustomersData(customersApiData);
             setFilteredCustomers(customersApiData);
-<<<<<<< HEAD
-            // console.log("c8ster", customersApiData)
-=======
             //console.log("c8ster", customersApiData)
->>>>>>> 557d27588a3f85e083206716693b0bf76a9dfa33
         } else {
             MessagesService.commonMessage(homeApiRes.message)
         }
@@ -109,7 +101,7 @@ export const Home = ({ navigation }: HomeScreenProps) => {
                 </View>
 
                 <View style={{ flexDirection: "column", alignItems: "center", position: "relative" }}>
-                    <Text style={item.transaction_type === 'CREDIT' ? { color: "green", fontSize: 18, fontWeight: "900" } : { fontSize: 18, fontWeight: "900", color: "red" }}>₹ {item.amount}</Text>
+                    <Text style={item.transaction_type === 'Credit' ? { color: "green", fontSize: 18, fontWeight: "900" } : { fontSize: 18, fontWeight: "900", color: "red" }}>₹ {item.amount}</Text>
                     <Text style={[styles.type, { color: colors.title }]}>{item.transaction_type}</Text>
                 </View>
 
@@ -126,8 +118,8 @@ export const Home = ({ navigation }: HomeScreenProps) => {
                 <View style={[GlobalStyleSheet.container, { paddingHorizontal: 30, padding: 0, paddingTop: 30 }]}>
                     <View style={[GlobalStyleSheet.flex]}>
                         <View>
-                            <Text style={{ ...FONTS.fontRegular, fontSize: 14, color: colors.title }}>Namaste</Text>
-                            <Text style={{ ...FONTS.fontSemiBold, fontSize: 24, color: colors.title }}>{userName}</Text>
+                            <Text style={{ ...FONTS.fontRegular, fontSize: 14, color: colors.title }}>Good Morning</Text>
+                            <Text style={{ ...FONTS.fontSemiBold, fontSize: 24, color: colors.title }}>Williams</Text>
                         </View>
                         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                             <TouchableOpacity
@@ -182,10 +174,10 @@ export const Home = ({ navigation }: HomeScreenProps) => {
             <ScrollView showsVerticalScrollIndicator={false}>
 
 
-                <View style={{ flex: 1, alignItems: 'center', }} >
+                <View style={{ flex: 1, alignItems: 'center' }} >
                     <View style={{
                         height: 140,
-                        width: 380,
+                        width: 400,
                         top: 20,
                         backgroundColor: COLORS.primary,
                         borderRadius: 31,
@@ -196,12 +188,11 @@ export const Home = ({ navigation }: HomeScreenProps) => {
                         },
                         shadowOpacity: 0.34,
                         shadowRadius: 31.27,
-                        elevation: 8,
-                        flexDirection: 'column'
+                        elevation: 8, flexDirection: 'column'
                     }}>
 
 
-                        <View style={{ width: 380, flexDirection: 'row', marginTop: 22, rowGap: 4, justifyContent: 'center', borderBlockColor: COLORS.inputborder, borderBottomWidth: 1, padding: 10 }}>
+                        <View style={{ width: 400, flexDirection: 'row', marginTop: 22, rowGap: 4, justifyContent: 'center', borderBlockColor: COLORS.white, borderBottomWidth: 1, padding: 10 }}>
                             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', borderRightWidth: 1, borderRightColor: COLORS.white }}>
                                 <Text style={{ ...FONTS.fontBold, fontSize: SIZES.h6, color: COLORS.primaryLight }}>Credit Amt.</Text>
                                 <Text style={{ ...FONTS.fontSemiBold, fontSize: SIZES.h3, color: COLORS.secondary }}>₹ {homeBanner?.credit}</Text>
