02b260efe72ad8adc0dabdf65319b15edf32d906
ARBAZ <arbaz@Arbaz.local>
2024-11-20 20:05:19 +0530

    release 28

--- app/constants/config.tsx
+++ app/constants/config.tsx
@@ -18,13 +18,13 @@ const CONFIG = {
     },
     APP_BUILD: {
         ANDROID: {
-            APP_VERSION: 27,
-            APP_VERSION_NAME: '1.0.27',
+            APP_VERSION: 28,
+            APP_VERSION_NAME: '1.0.28',
             APP_URL: "https://play.google.com/store/apps/details?id=com.paylap.paylapscore"
         },
         IOS: {
-            APP_VERSION: 27,
-            APP_VERSION_NAME: '1.0.27',
+            APP_VERSION: 28,
+            APP_VERSION_NAME: '1.0.28',
             APP_URL: "https://apps.apple.com/us/app/paylap-score/id6736965791"
         }
     },
