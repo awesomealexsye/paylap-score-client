b6ef00c887089eea5b868a6285b26771a25fc890
Arbaz Khan <arbazkhan@Arbazs-MacBook-Air.local>
2025-04-02 16:10:13 +0530

    release 51

--- app/constants/config.tsx
+++ app/constants/config.tsx
@@ -35,13 +35,13 @@ const CONFIG = {
     },
     APP_BUILD: {
         ANDROID: {
-            APP_VERSION: 50,
-            APP_VERSION_NAME: '1.0.50',
+            APP_VERSION: 51,
+            APP_VERSION_NAME: '1.0.51',
             APP_URL: "https://play.google.com/store/apps/details?id=com.paylap.paylapscore"
         },
         IOS: {
-            APP_VERSION: 50,
-            APP_VERSION_NAME: '1.0.50',
+            APP_VERSION: 51,
+            APP_VERSION_NAME: '1.0.51',
             APP_URL: "https://apps.apple.com/us/app/paylap-score/id6736965791"
         }
     },
